c52e3474788870b21fa779ee9b1b843c807ff072
Stephen Willey <stephen.willey@primefocusworld.com>
2011-02-18 09:38:30 +0000

    Make sure script is saved before submission

--- plugins/nuke/sge4vfx_nuke.py
+++ plugins/nuke/sge4vfx_nuke.py
@@ -21,82 +21,84 @@ def Submit(fullSize, startFrame, endFrame,
 	# Specify where gridsub is
 	gridsub = "gridsub" # assumes it's in the path, otherwise put full path
 
-	# Get all the job bits
-	script_name=nuke.Root().name()
-	job_path=os.path.basename(script_name)
-	(job_title, extension)=os.path.splitext(job_path)
-	dirPath = nukescripts.script_directory();
+	# Save the script
+	if nuke.scriptSave():
+		# Get all the job bits
+		script_name=nuke.Root().name()
+		job_path=os.path.basename(script_name)
+		(job_title, extension)=os.path.splitext(job_path)
+		dirPath = nukescripts.script_directory();
 	
-	# Build a date string to save off the SGE submission stuff
-	dt = datetime.today()
-	tt = dt.timetuple()
-	dateStr = (str(tt[0]) + str(tt[1]).zfill(2) + str(tt[2]).zfill(2) +
-		"-" + str(tt[3]).zfill(2) + str(tt[4]).zfill(2) +
-		str(tt[5]).zfill(2))
+		# Build a date string to save off the SGE submission stuff
+		dt = datetime.today()
+		tt = dt.timetuple()
+		dateStr = (str(tt[0]) + str(tt[1]).zfill(2) + str(tt[2]).zfill(2) +
+			"-" + str(tt[3]).zfill(2) + str(tt[4]).zfill(2) +
+			str(tt[5]).zfill(2))
 	
-	# Create the SGE folder next to the Nuke script if it doesn't exist
-	# This also creates the log folder for the stdout and stderr
-	sgePath = dirPath + "/theQ/" + dateStr
-	prepCmd1 = ("if [ ! -d " + sgePath + " ]; then mkdir -p "
-		+ sgePath + "/logs; fi")
-	os.system(prepCmd1)
+		# Create the SGE folder next to the Nuke script if it doesn't exist
+		# This also creates the log folder for the stdout and stderr
+		sgePath = dirPath + "/theQ/" + dateStr
+		prepCmd1 = ("if [ ! -d " + sgePath + " ]; then mkdir -p "
+			+ sgePath + "/logs; fi")
+		os.system(prepCmd1)
 
-	# Check whether to force full size
-	if fullSize:
-		fullSizeBit = "-f "
-	else:
-		fullSizeBit = ""
+		# Check whether to force full size
+		if fullSize:
+			fullSizeBit = "-f "
+		else:
+			fullSizeBit = ""
 
-	# If it's a batch job, do batch stuff...
-	if int(batchSize) > 1:
-		frameRangeBit = "-F ${SGE_TASK_ID}-${ENDFRAME} "
-	else:
-		frameRangeBit = "-F ${SGE_TASK_ID} "
-	nukeCmd = ("nuke -x " + fullSizeBit + frameRangeBit + sgePath +
-		"/" + job_path)
+		# If it's a batch job, do batch stuff...
+		if int(batchSize) > 1:
+			frameRangeBit = "-F ${SGE_TASK_ID}-${ENDFRAME} "
+		else:
+			frameRangeBit = "-F ${SGE_TASK_ID} "
+		nukeCmd = ("nuke -x " + fullSizeBit + frameRangeBit + sgePath +
+			"/" + job_path)
 
-	nf = open(sgePath + "/nukeCommand.sh", "w")
-	nf.write("#!/bin/bash\n\n")
-	nf.write("#$ -o " + sgePath + "/logs/o_$TASK_ID\n")
-	nf.write("#$ -e " + sgePath + "/logs/e_$TASK_ID\n\n")
-	# I put the batch endframe calculation stuff into the script either way
-	# If it's not a batch job, ENDFRAME will just never be used by the
-	# Nuke command on the following lines
-	nf.write("let ENDFRAME=${SGE_TASK_ID}+${SGE_TASK_STEPSIZE}\n")
-	nf.write("if [ ${ENDFRAME} -gt ${SGE_TASK_LAST} ]; then\n")
-	nf.write("	ENDFRAME=${SGE_TASK_LAST}\n")
-	nf.write("fi\n\n")
-	nf.write(nukeCmd + "\n")
-	nf.write("\n# Write return code for epilog script\n")
-	nf.write("echo $? > /tmp/${JOB_ID}-${SGE_TASK_ID}-return\n")
-	nf.close()
-	os.chmod(sgePath + "/nukeCommand.sh", 0755)
+		nf = open(sgePath + "/nukeCommand.sh", "w")
+		nf.write("#!/bin/bash\n\n")
+		nf.write("#$ -o " + sgePath + "/logs/o_$TASK_ID\n")
+		nf.write("#$ -e " + sgePath + "/logs/e_$TASK_ID\n\n")
+		# I put the batch endframe calculation stuff into the script either way
+		# If it's not a batch job, ENDFRAME will just never be used by the
+		# Nuke command on the following lines
+		nf.write("let ENDFRAME=${SGE_TASK_ID}+${SGE_TASK_STEPSIZE}\n")
+		nf.write("if [ ${ENDFRAME} -gt ${SGE_TASK_LAST} ]; then\n")
+		nf.write("	ENDFRAME=${SGE_TASK_LAST}\n")
+		nf.write("fi\n\n")
+		nf.write(nukeCmd + "\n")
+		nf.write("\n# Write return code for epilog script\n")
+		nf.write("echo $? > /tmp/${JOB_ID}-${SGE_TASK_ID}-return\n")
+		nf.close()
+		os.chmod(sgePath + "/nukeCommand.sh", 0755)
 
-	# Now the actual gridsub execution thingy
-	sgeCmd = (gridsub
-		+ " -N " + job_title
-		+ " -V -S /bin/bash "
-		+ " -pe pe1 " + slotsPerFrame
-		+ " -q " + whichQueue
-		+ " -t " + startFrame + "-" + endFrame + ":" + batchSize + " "
-		+ sgePath
-		+ "/nukeCommand.sh")
-	sf = open(sgePath + "/sgeNuke.sh", "w")
-	sf.write("#!/bin/bash\n\n")
-	sf.write(sgeCmd + "\n")
-	sf.close()
-	os.chmod(sgePath + "/sgeNuke.sh", 0755)
+		# Now the actual gridsub execution thingy
+		sgeCmd = (gridsub
+			+ " -N " + job_title
+			+ " -V -S /bin/bash "
+			+ " -pe pe1 " + slotsPerFrame
+			+ " -q " + whichQueue
+			+ " -t " + startFrame + "-" + endFrame + ":" + batchSize + " "
+			+ sgePath
+			+ "/nukeCommand.sh")
+		sf = open(sgePath + "/sgeNuke.sh", "w")
+		sf.write("#!/bin/bash\n\n")
+		sf.write(sgeCmd + "\n")
+		sf.close()
+		os.chmod(sgePath + "/sgeNuke.sh", 0755)
 
-	# Take a snapshot of the Nuke file (useful for resubmission)
-	shutil.copyfile(script_name, sgePath + "/" + job_path)
+		# Take a snapshot of the Nuke file (useful for resubmission)
+		shutil.copyfile(script_name, sgePath + "/" + job_path)
 
-	# Run the SGE submission command
-	p1 = Popen(sgePath + "/sgeNuke.sh", stdout=PIPE)
-	theJobID = p1.communicate()[0]
+		# Run the SGE submission command
+		p1 = Popen(sgePath + "/sgeNuke.sh", stdout=PIPE)
+		theJobID = p1.communicate()[0]
 	
-	# Tell the user the SGE ID
-	sgeID = theJobID.partition(".")[0]
-	nuke.message("Render job submitted\nID is " + sgeID)
+		# Tell the user the SGE ID
+		sgeID = theJobID.partition(".")[0]
+		nuke.message("Render job submitted\nID is " + sgeID)
 
 
 # Creates the UI panel that the user sees
@@ -118,6 +120,7 @@ def RenderPanel():
 	p.addSingleLineInput("Slots per frame:", slotsRequired)
 	p.addSingleLineInput("Queue:", whichQueue)
 	p.addSingleLineInput("Batch Size:", batchSize)
+	p.addNotepad("Important:", "When you click OK, this will save your script in it's current form.\n\nIf you don't want your file overwritten now, click Cancel.")
 	p.addButton("Cancel")
 	p.addButton("OK")
 
