75cc67a4da3c4f93180f028e15838c37fbc32bc9
Stephen Willey <stephen.willey@primefocusworld.com>
2011-11-18 17:05:03 +0000

    Removed shell handler from urls

--- webui/urls.py
+++ webui/urls.py
@@ -1,13 +1,11 @@
 from handlers.simple import IndexHandler
 from handlers.jobs import MainJobTable
-from handlers.shelltest import ShellHandler
 from handlers.queueStats import QueueStatsHandler
 from handlers.quotas import QuotaHandler
 
 url_patterns = [
     (r"/", IndexHandler),
     (r"/jobs", MainJobTable),
-    (r"/shell", ShellHandler),
     (r"/queueStats", QueueStatsHandler),
     (r"/quotas", QuotaHandler),
 ]
